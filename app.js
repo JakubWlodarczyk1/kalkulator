991df7b47be9f3aeb99e379b13722688584c09d9
Jakub Włodarczyk <60402277+JakubWlodarczyk1@users.noreply.github.com>
2023-03-27 20:29:01 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display");
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+function calculateResult() {
+    try 
+    {
+      display.value = eval(display.value);
+    } 
+    catch (error) 
+    {
+      display.value = "Error";
+      setTimeout(clearDisplay, 1000);
+    }
+}
